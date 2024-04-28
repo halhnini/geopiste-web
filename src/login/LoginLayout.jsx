6d96e1fe089231d9d4c87d6bfaf9b72da4cd1866
HALHNINI <mohammed.halhnini@swibeco.ch>
2024-04-28 12:18:58 +0100

    change images and colors

--- src/login/LoginLayout.jsx
+++ src/login/LoginLayout.jsx
@@ -13,7 +13,7 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    background: theme.palette.primary.main,
+    background: '#9da0d5',
     paddingBottom: theme.spacing(5),
     width: theme.dimensions.sidebarWidth,
     [theme.breakpoints.down('lg')]: {
